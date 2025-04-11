c7a8f03ad1fcc90043c87ee457d274f074ca0229
Hans <hschwaiger@usgs.gov>
2025-04-10 17:31:59 -0800

    Added control script to generate 1-hour contours of ashfall arrival time.

--- scripts/GFSVolc_to_gif_dp_mm.sh
+++ scripts/GFSVolc_to_gif_dp_mm.sh
@@ -462,7 +462,12 @@ composite -geometry +${legendx_UL}+${legendy_UL} ${ASH3DSHARE_PP}/legend_dep.png
 # Finalizing output (animations, shape files, etc.)
 #Make shapefile
 echo "Generating shapefile with Ash3d_PostProc 5 5"
-/opt/USGS/Ash3d/bin/Ash3d_PostProc 3d_tephra_fall.nc 5 5
+# This first line uses the default contour levels
+#/opt/USGS/Ash3d/bin/Ash3d_PostProc 3d_tephra_fall.nc 5 5
+# This one uses a control file setting 1-hour contour levels
+cp ${ASH3DSCRIPTDIR}/pp_ashfalltime_shp.ctr .
+${ASH3DBINDIR}/Ash3d_PostProc pp_ashfalltime_shp.ctr
+
 mv depothik.zip dp_mm_shp.zip
 # Clean up more temporary files
 if [ "$CLEANFILES" == "T" ]; then
